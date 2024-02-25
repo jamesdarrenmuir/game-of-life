4b1bb0ed1dd9ea72b53680ba8c9a4f2105dd00e0
James Darren Muir <james@jamesdarrenmuir.com>
2024-02-25 00:22:11 -0800

    add command-line arguments

--- game_of_life.py
+++ game_of_life.py
@@ -1,47 +1,79 @@
-import numpy as np
+import argparse
+
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.signal import convolve
 
-rng = np.random.default_rng()
+parser = argparse.ArgumentParser(
+    prog="Conway's Game of Life",
+    description="Simulation of Conway's Game of Life using NumPy, SciPy, and Matplotlib.",
+    epilog="Created by James Muir.",
+)
 
-BOARD_SIZE = 100
-# 
-# board = np.zeros([BOARD_SIZE, BOARD_SIZE])
-# board = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=int)
-board = rng.integers(1, size=(BOARD_SIZE, BOARD_SIZE), dtype=bool, endpoint=True)
-# print(f"{board=}")
+parser.add_argument(
+    "-r",
+    "--rate",
+    help="simulation rate (Hz)",
+    metavar="f",
+    default=1,
+    type=int,
+)
 
-neighbors_kernel = np.array([[1, 1, 1],
-                            [1, 0, 1],
-                            [1, 1, 1]])
+parser.add_argument(
+    "-i",
+    "--init",
+    choices=["random", "file"],
+    help="source of the initial board state",
+    default="random",
+    metavar="source"
+)
+
+parser.add_argument(
+    "-s",
+    "--size",
+    help="number of cells on one side of the square board",
+    metavar="N",
+    default=30,
+    type=int,
+)
+
+parser.add_argument(
+    "-f",
+    "--file",
+    help="image file to use as initial board state",
+    metavar="/path/to/file",
+)
+
+args = parser.parse_args()
+
+if args.init == "random":
+    rng = np.random.default_rng()
+    board = rng.integers(1, size=(args.size, args.size), dtype=bool, endpoint=True)
+
+if args.init == "file":
+    # negate image to make black pixels True
+    board = ~plt.imread(args.file).astype(bool)
+
+neighbors_kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
 # make program stop when figure is closed
 running = True
+
+
 def close(event):
     global running
     running = False
+
+
 fig, ax = plt.subplots()
 fig.canvas.mpl_connect("close_event", close)
 
 # main loop
 while running:
-    print(f"{running=}")
     # display board
     ax.imshow(board, cmap="binary")
-    plt.pause(1)
+    plt.pause(1/args.rate)
 
     # calculate new board
     neighbors = convolve(board, neighbors_kernel, mode="same")
     board = board * (neighbors == 2) + (neighbors == 3)
-    
-    # print(f"{neighbors=}")
-
